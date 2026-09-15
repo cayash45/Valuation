5607f6bca689346871716760b5826e60012e9078
cayash45 <cayash77@gmail.com>
2026-09-15 18:41:18 +0530

    app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import streamlit as st
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import pandas as pd
+import numpy as np
 
 from data_fetcher import (
     fetch_index_data,
